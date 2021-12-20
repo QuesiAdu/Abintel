97248d0bf6c52865f7dc03da6c57128ebdb1e274
pappy04 <adugyamfijp@hotmail.com>
2021-12-20 12:33:23 +0000

    fixed navbar error on mobile screen

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-  });
-  $('.loop').owlCarousel({
-    center: true,
-    items:1,
-    loop:true,
-    margin:10,
-    nav:true
-});
 
 var scrollpos = window.scrollY;
 var header = document.getElementById("header");
